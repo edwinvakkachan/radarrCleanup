880096102fb7a171218b4887a066c32a93f46278
edwin vakkachan <edwinvakkachan@gmail.com>
2026-02-12 19:50:45 +0530

    error fix

--- server.js
+++ server.js
@@ -161,7 +161,7 @@ try {
   if(value?.statusMessages?.[0]?.messages?.[0] == 'Movie title mismatch, automatic import is not possible. Manual Import required.') {
        if (/malayalam|mal/i.test(value.title.toLowerCase())) {
     console.log("🚨 Please remove Manualy ", value.title);
-    await sendTelegramMessage(`🚨 Please remove Manualy ${value.title}`)
+    await sendTelegramMessage("🚨 Please remove Manualy")
     await sendTelegramMessage(value.title)
     continue;
 }   
